8710390f9d0b9af0e4e6c05445dc2b5fc5d2d6ca
tqz <572393128@qq.com>
2019-08-17 17:18:27 +0800

    基础组件搞定

--- src/components/Home/Home.vue
+++ src/components/Home/Home.vue
@@ -1,15 +1,21 @@
 <template>
   <div>
     <Header></Header>
+    <Section></Section>
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
 import Header from "../base/Header/Header";
+import Section from "../base/Section/Section";
+import Footer from "../base/Footer/Footer";
 
 export default {
   components: {
-    Header
+    Header,
+    Section,
+    Footer
   }
 };
 </script>
