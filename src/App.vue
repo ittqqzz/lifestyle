c607fb53112cc5a3bb74e8098a3c62a57131281c
tqz <572393128@qq.com>
2019-08-18 19:25:42 +0800

    home

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
 <template>
   <div id="app">
+    <Header></Header>
     <router-view/>
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
+import Header from "./components/base/Header/Header";
+import Footer from "./components/base/Footer/Footer";
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Header,
+    Footer
+  }
 }
 </script>
 
--- src/components/Home/Home.vue
+++ src/components/Home/Home.vue
@@ -1,21 +1,15 @@
 <template>
   <div>
-    <Header></Header>
     <Section></Section>
-    <Footer></Footer>
   </div>
 </template>
 
 <script>
-import Header from "../base/Header/Header";
 import Section from "../base/Section/Section";
-import Footer from "../base/Footer/Footer";
 
 export default {
   components: {
-    Header,
-    Section,
-    Footer
+    Section
   }
 };
 </script>
